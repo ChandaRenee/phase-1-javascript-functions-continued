577fbcf449e891cde36baee406e60c18d56088d0
Chanda Hopkins <chandahopkins@outlook.com>
2023-05-16 13:10:29 -0500

    functions continued complete

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ const mondayWork = function(work = 'go to the office') {
     return `This Monday, I will ${work}.`
 }
 
-
+function wrapAdjective(string = "*") {
+    return function(value2 = "special") {
+        return `You are ${string}${value2}${string}!`
+    }
+} 
  
